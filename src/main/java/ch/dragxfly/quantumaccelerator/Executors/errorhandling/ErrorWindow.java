53f89e1656e304b6f3e40c9014009f7907a93ce9
Dragxfly <59048803+JannisJost@users.noreply.github.com>
2021-06-05 18:07:57 +0200

    Added fxml to display errors

--- src/main/java/ch/dragxfly/quantumaccelerator/Executors/errorhandling/ErrorWindow.java
+++ src/main/java/ch/dragxfly/quantumaccelerator/Executors/errorhandling/ErrorWindow.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -13,15 +14,17 @@ import javafx.stage.Stage;
 public class ErrorWindow {
 
     public void showErrorWindow(String errorMessage) {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Error.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/popup/Error.fxml"));
         ErrorController controller = new ErrorController();
         loader.setController(controller);
-        controller.setErrorMessage(errorMessage);
         try {
             Scene scene = new Scene(loader.load());
             Stage stage = new Stage();
             stage.setScene(scene);
+            stage.initStyle(StageStyle.UNDECORATED);
             stage.show();
+            controller.setErrorMessage(errorMessage);
+            controller.setTheme();
         } catch (IOException ex) {
         }
 
