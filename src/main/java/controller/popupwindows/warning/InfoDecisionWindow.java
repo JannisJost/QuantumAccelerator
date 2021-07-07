3b4cc183afc70a02963b6366d12bb9c8bb01173a
Dragxfly <59048803+JannisJost@users.noreply.github.com>
2021-07-07 15:55:15 +0200

    added different info windows

--- src/main/java/controller/popupwindows/warning/InfoDecisionWindow.java
+++ src/main/java/controller/popupwindows/warning/InfoDecisionWindow.java
@@ -2,25 +2,33 @@ package controller.popupwindows.warning;
 
 import ch.dragxfly.quantumaccelerator.views.MultilingualView;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.ImageView;
 
 /**
  *
  * @author jannis
  */
-public class InfoWindow extends MultilingualView {
+public class InfoDecisionWindow extends MultilingualView {
 
-    public void ShowInfoWindow(String infoMessage) {
+    public boolean ShowInfoWindow(String infoMessage) {
         Locale locale = new Locale(super.getLanguage());
         ResourceBundle bundle = ResourceBundle.getBundle("languages.warnings.warnings", locale);
-        Alert info = new Alert(Alert.AlertType.INFORMATION);
+        Alert info = new Alert(AlertType.CONFIRMATION);
         info.setTitle(bundle.getString("info"));
-        info.setHeaderText(bundle.getString("info"));
+        info.setHeaderText(bundle.getString("rUSure"));
         info.setContentText(infoMessage);
         info.setGraphic(new ImageView(this.getClass().getResource("/styles/icons/popup/Info.png").toString()));
-        info.showAndWait();
+        info.getButtonTypes().clear();
+        ButtonType ok = new ButtonType(bundle.getString("btnContinue"));
+        ButtonType cancel = new ButtonType(bundle.getString("btnCancel"));
+        info.getButtonTypes().addAll(ok, cancel);
+        Optional<ButtonType> result = info.showAndWait();
+        return result.get() == ok;
     }
 
     @Override
