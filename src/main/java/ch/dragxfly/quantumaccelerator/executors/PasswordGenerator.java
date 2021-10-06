eaf89b87eb28843dfcfc21aa0ef0b6708532896e
Dragxfly <59048803+JannisJost@users.noreply.github.com>
2021-10-06 09:33:51 +0200

    Added more characters for passwords and improved code readability

--- src/main/java/ch/dragxfly/quantumaccelerator/executors/PasswordGenerator.java
+++ src/main/java/ch/dragxfly/quantumaccelerator/executors/PasswordGenerator.java
@@ -9,25 +9,22 @@ import java.util.Random;
 
 /**
  *
- * @author janni
+ * @author jannis
  */
 public class PasswordGenerator {
 
     private final PasswordGeneratorController controller;
-    private int pwLength = 0;
-    private int currentPwLength = 0;
-    private double mouseX;
-    private double mouseY;
+    private int currentPwLength = 0, requestedPwLength = 0;
+    private double mouseX, mouseY;
     private String password = "";
-    private final double ARRAY_SYMBOLS_SIZE;
-    private final double ARRAY_SYMBOLS_PERCENT;
+    private final double ARRAY_SYMBOLS_SIZE, ARRAY_SYMBOLS_PERCENT;
     private final BigDecimal bgXMultYPercent;
     private boolean isRunning = false;
+    private final ArrayList<Character> symbolsAsList;
     private final Character symbols[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        '$', '_', '?', '!', '%', ')', '(', '/','+'};
-    private final ArrayList<Character> symbolsAsList;
+        '$', '_', '?', '!', '%', ')', '(', '/', '+', '*', '#', '[', ']', ':'};
 
     public PasswordGenerator(PasswordGeneratorController controller) {
         symbolsAsList = new ArrayList<>(Arrays.asList(symbols));
@@ -45,17 +42,17 @@ public class PasswordGenerator {
 
     public void startGenerating(int passwordLength) {
         isRunning = true;
-        this.pwLength = passwordLength;
+        this.requestedPwLength = passwordLength;
         while (currentPwLength < passwordLength) {
             try {
                 Thread.sleep(getRandomTime());
                 if (controller.isIsMouseOnRectangle()) {
                     requestMouseCoordinates();
-                    char nextChar = coordinatesIntoChar(mouseX, mouseY);
-                    if (password.endsWith(Character.toString(nextChar))) {
+                    char nextPwChar = coordinatesIntoChar(mouseX, mouseY);
+                    if (password.endsWith(Character.toString(nextPwChar))) {
                         //do nothing
                     } else {
-                        password += nextChar;
+                        password += nextPwChar;
                         currentPwLength = password.length();
                         controller.setPasswordText(password);
                     }
@@ -73,18 +70,18 @@ public class PasswordGenerator {
     }
 
     private char coordinatesIntoChar(double mouseX, double mouseY) {
-        //Calculates the mouse coordinates into a char 
+        //Calculates the mouse coordinates into a char
         double xTimesY = mouseX * mouseY;
-        BigDecimal XMultipliedWithY = new BigDecimal(xTimesY);
-        BigDecimal xTimesYOfMax = XMultipliedWithY.multiply(bgXMultYPercent);
+        BigDecimal xMultipliedWithY = new BigDecimal(xTimesY);
+        BigDecimal xTimesYOfMax = xMultipliedWithY.multiply(bgXMultYPercent);
         //Rounds to next integer (index in array) when converted from percent
         BigDecimal percentageOfArray = new BigDecimal(ARRAY_SYMBOLS_PERCENT * (Math.ceil(Math.abs(xTimesYOfMax.doubleValue() / ARRAY_SYMBOLS_PERCENT))));
         //From percent to integer (index)
-        BigDecimal arrayIndex = percentageOfArray.multiply(new BigDecimal(ARRAY_SYMBOLS_SIZE)).divide(new BigDecimal(100));
-        return symbolsAsList.get(arrayIndex.intValue());
+        BigDecimal index = percentageOfArray.multiply(new BigDecimal(ARRAY_SYMBOLS_SIZE)).divide(new BigDecimal(100));
+        return symbolsAsList.get(index.intValue());
     }
 
-    public boolean isIsRunning() {
+    public boolean isRunning() {
         return isRunning;
     }
 
